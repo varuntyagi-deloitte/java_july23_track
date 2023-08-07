9d21ccf8e309724cf0181596d5b7e8f5f9e818fd
Tyagi <varutyagi@deloitte.com>
2023-08-07 06:27:29 +0530

    Password Validator file is added:

--- src/main/java/utilities/PasswordValidator.java
+++ src/main/java/utilities/PasswordValidator.java
@@ -1,2 +1,36 @@
-package utilities;public class PasswordValidator {
+package utilities;
+
+public class PasswordValidator {
+    public static boolean isPasswordValid(String password) {
+        if (password == null) {
+            return false;
+        }
+
+        // Checks minimum length
+        if (!((password.length() >=5)&&(password.length()<=10))) {
+            return false;
+        }
+
+        // Checks for at least one uppercase letter
+        if (!password.matches(".*[A-Z].*")) {
+            return false;
+        }
+
+        // Checks for at least one lowercase letter
+        if (!password.matches(".*[a-z].*")) {
+            return false;
+        }
+
+        // Checks for at least one digit
+        if (!password.matches(".*\\d.*")) {
+            return false;
+        }
+
+        // Checks for at least one special character
+        if (!password.matches(".*[@#$%^&+=].*")) {
+            return false;
+        }
+
+        return true;
+    }
 }
